1b132f36f3fe21a3898be0884d4cc7a46e8ea8c1
TheBoringDude <iamcoderx@gmail.com>
2021-11-25 19:17:01 +0800

    add github org link

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,19 @@
 <template>
   <header class="py-2 bg-warmGray-900">
-    <div class="mx-auto w-11/12">
+    <div class="mx-auto w-11/12 flex flex-wrap items-center justify-between">
       <div class="inline-flex items-end">
         <img src="@/assets/logo.png" class="h-20 object-contain" />
 
         <h1 class="text-xl font-black text-white mb-3">DEV</h1>
       </div>
+
+      <a
+        href="https://github.com/World-of-Cryptopups/"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-gray-200 text-sm font-bold tracking-wide hover:underline"
+        >Github</a
+      >
     </div>
   </header>
 </template>
